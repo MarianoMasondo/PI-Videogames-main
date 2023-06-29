ff16ae5b49bda70929388afadc8cdf7c64bae2ba
Mariano Masondo <nanomason@gmail.com>
2023-06-29 01:26:24 -0300

    Comienzo de front

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import{ Route} from "react-router-dom";
-import landingPage from './views/landingPage/landingPage.jsx';
+import React from 'react';// ver
+import{ Route, Routes} from "react-router-dom";
+import LandingPage from './views/landingPage/landingPage.jsx';
 
 function App() {
   return (
     <div className="App">
-    
-      <Route exact path= "/" element={<landingPage />} />
-   
+    <Routes>
+      <Route path= "/" element={<LandingPage />} />
+    </Routes>   
+    <h1>holanda</h1>
     </div>
   );
 }
--- client/src/views/landingPage/landingPage.jsx
+++ client/src/views/landingPage/landingPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./landingPage.module.css"
 
 export default function landingPage(props){
     return(
         <div className={styles.container}>
             <h1>Sphere</h1>
-            <h3>Welcome to the games world</h3>
-            <img src="" alt="background image" />
-            <button onClick={props.ingresar}>Ingresar</button>
+            <h3>Welcome to the games world</h3>   
+            <NavLink to ="/home">
+            <button>Ingresar</button>
+            </NavLink>       
         </div>
     );
 }
